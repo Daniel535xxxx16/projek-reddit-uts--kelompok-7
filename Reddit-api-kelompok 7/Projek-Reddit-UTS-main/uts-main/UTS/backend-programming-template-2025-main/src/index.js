871b45ee4a64fe516a5c9175019925ea6f89f5e2
Justin24137 <justin.benedikt4@gmail.com>
2025-04-09 08:21:52 +0700

    kurang `

--- Reddit-api-kelompok 7/Projek-Reddit-UTS-main/uts-main/UTS/backend-programming-template-2025-main/src/index.js
+++ Reddit-api-kelompok 7/Projek-Reddit-UTS-main/uts-main/UTS/backend-programming-template-2025-main/src/index.js
@@ -2,5 +2,5 @@ const app = require('./core/server');
 const config = require('./core/config');
 
 app.listen(config.port, () => {
-  console.log(Server running on http://localhost:${config.port});
+  console.log('Server running on http://localhost:${config.port}');
 });
