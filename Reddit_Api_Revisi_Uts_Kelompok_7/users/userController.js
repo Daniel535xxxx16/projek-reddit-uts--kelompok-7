422fdb5a2212945c779b96fbdabff18ffbb665b5
belutamazon <f.alvitio112@gmail.com>
2025-04-22 21:13:11 +0700

    Update struktur code

--- Reddit_Api_Revisi_Uts_Kelompok_7/users/userController.js
+++ Reddit_Api_Revisi_Uts_Kelompok_7/users/userController.js
@@ -1,20 +1,29 @@
 const User = require('./user');
 
-exports.register = async (req, res) => {
+//Endpoint 8
+async function register (req, res){
   const newUser = new User(req.body);
   await newUser.save();
   res.status(201).json(newUser);
 };
 
-exports.login = async (req, res) => {
+//Endpoint 9
+async function login (req, res){
   const user = await User.findOne({ username: req.body.username, password: req.body.password });
   if (!user) return res.send('Invalid Username or Password');
   setTimeout(() =>{res.send(`Login Success. Welcome ${user.username}.`)},2000)};
 
-exports.getProfile = async (req, res) => {
+//Enpoint 10
+async function getProfile (req, res){
   const user = await User.findById(req.params.id);
   if(!user) return res.send('Cannot find user');
   res.send({username: user.username, 
             email: user.email, 
             joinedAt : user.joinedAt});
 };
+
+module.exports = {
+  register,
+  login,
+  getProfile
+}
