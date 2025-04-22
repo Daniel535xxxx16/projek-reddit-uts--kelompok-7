759c476da3fad0ed77f0d988c49999411e20790c
belutamazon <f.alvitio112@gmail.com>
2025-04-22 21:12:38 +0700

    Update struktur code

--- Reddit_Api_Revisi_Uts_Kelompok_7/post/postController.js
+++ Reddit_Api_Revisi_Uts_Kelompok_7/post/postController.js
@@ -1,27 +1,40 @@
 const Post = require('./post');
 
-exports.getPosts = async (req, res) => {
+//Endpoint 3
+async function getPosts(req, res){
   const posts = await Post.find();
   res.json(posts);
 };
 
-exports.createPost = async (req, res) => {
+//Endpoint 4
+async function createPost(req, res){
   const newPost = new Post(req.body);
   await newPost.save();
   res.status(201).json(newPost);
 };
 
-exports.upvotePost = async (req, res) => {
+//Endpoint 5
+async function upvotePost(req, res){
   const post = await Post.findByIdAndUpdate(req.params.id, { $inc: { upvotes: 1 } }, { new: true });
   res.json(post);
 };
 
-exports.downvotePost = async (req, res) => {
+//Endpoint 6
+async function downvotePost(req, res){
   const post = await Post.findByIdAndUpdate(req.params.id, { $inc: { downvotes: 1 } }, { new: true });
   res.json(post);
 };
 
-exports.deletePost = async (req, res) => {
+//Endpoint 7
+async function deletePost(req, res){
   await Post.findByIdAndDelete(req.params.id);
   res.json({ message: 'Post deleted' });
 };
+
+module.exports = {
+  getPosts,
+  createPost,
+  upvotePost,
+  downvotePost,
+  deletePost
+}
