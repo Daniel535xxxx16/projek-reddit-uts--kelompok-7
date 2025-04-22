bc19d0d90ce8651e6a71fffe259dc5ad6087e38c
belutamazon <f.alvitio112@gmail.com>
2025-04-22 21:11:52 +0700

    Update struktur code

--- Reddit_Api_Revisi_Uts_Kelompok_7/comment/commentController.js
+++ Reddit_Api_Revisi_Uts_Kelompok_7/comment/commentController.js
@@ -1,11 +1,13 @@
 const Comment = require('./comment');
 
-exports.getComments = async (req, res) => {
+//Endpoint 1
+async function getComments(req, res){
   const comments = await Comment.find({ postId: req.params.postId });
   res.json(comments);
 };
 
-exports.addComment = async (req, res) => {
+//Endpoint 2
+async function addComment(req, res){
   const newComment = new Comment({
     postId: req.params.postId,
     ...req.body
@@ -13,3 +15,8 @@ exports.addComment = async (req, res) => {
   await newComment.save();
   res.status(201).json(newComment);
 };
+
+module.exports = {
+  getComments,
+  addComment
+}
